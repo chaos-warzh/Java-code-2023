2d2cf80565bc7dd7de76975a09c67a70f719635c
chaos-warzh <haidedeyu@163.com>
2023-10-05 14:04:30 +0800

    + GuessNumber, + MyHeap

--- src/datastructures2/GuessNumber.java
+++ src/datastructures2/GuessNumber.java
@@ -4,7 +4,32 @@ public class GuessNumber {
   //check已在Tools.java中定义，你可以通过Tools.check(int)调用
   public static int guessNumber() {
     //write ur code here.
-    return 0;
+
+    if (Tools.check(Integer.MAX_VALUE) == 0) {
+      return Integer.MAX_VALUE;
+    }
+
+
+    Integer min = Integer.MIN_VALUE;
+    Integer max = Integer.MAX_VALUE;
+    while (true) {
+      Integer guess = 0;
+      if (max >> 31 == min >> 31) {
+        guess = (max - min) / 2 + min;
+      } else {
+        guess = (max + min) / 2;
+      }
+      switch (Tools.check(guess)) {
+        case 1:
+          max = guess;
+          break;
+        case -1:
+          min = guess;
+          break;
+        case 0:
+          return guess;
+      }
+    }
   }
 
   public static void main(String[] args) {
--- src/datastructures2/MyHeap.java
+++ src/datastructures2/MyHeap.java
@@ -12,22 +12,81 @@ public class MyHeap {
     size = 0;
   }
 
+  public void floatUp() {
+    int tmpIdx = arr.size() - 1;
+    while (true) {
+      if (tmpIdx == 0) {
+        return;
+      }
+      int dad = (tmpIdx - 1) / 2;
+
+      if (arr.get(tmpIdx) < arr.get(dad)) {
+        // swap
+        int tmp = arr.get(dad);
+        arr.set(dad, arr.get(tmpIdx));
+        arr.set(tmpIdx, tmp);
+        tmpIdx = dad;
+      } else {
+        return;
+      }
+
+    }
+  }
+
+
   public void push(int val) {
     //write ur code here.
+    arr.add(val);
+    // adjust
+    floatUp();
+  }
+
+  public void sink() {
+    int tmpIdx = 0;
+    while (true) {
+      int childL = 2 * tmpIdx + 1;
+      int childR = childL + 1;
+      int childMin = 0;
+
+      if (childR >= arr.size()) {
+        if (childL >= arr.size()) {
+          return;
+        } else {
+          childMin = childL;
+        }
+      } else {
+        childMin = arr.get(childL) < arr.get(childR) ? childL : childR; // let the minimum float up
+      }
+
+      if (arr.get(tmpIdx) > arr.get(childMin)) {
+        // swap
+        int tmp = arr.get(childMin);
+        arr.set(childMin, arr.get(tmpIdx));
+        arr.set(tmpIdx, tmp);
+        tmpIdx = childMin;
+      } else {
+        return;
+      }
+
+    }
   }
 
   public void pop() {
     //write ur code here.
+    arr.set(0, arr.get(arr.size() - 1));
+    arr.remove(arr.size() - 1);
+    // the 0 node -> sinking
+    sink();
   }
 
   public int top() {
     //write ur code here.
-    return 0;
+    return arr.get(0);
   }
 
   public int size() {
     //write ur code here.
-    return 0;
+    return arr.size();
   }
 
   public static void main(String[] args) {
